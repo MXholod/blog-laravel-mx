c84f5865c46762ab3d3bd8dab7c503f09898e8a9
Michael Holod <git_ara@ukr.net>
2021-08-19 01:19:41 +0300

    Model Post modified. Added values to the property 'fillable', method 'withTimestamps' and also 'getImage' method to check an image in View

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,6 +12,9 @@ class Post extends Model
 {
     use HasFactory;
 	
+	//For mass assignment
+	protected $fillable = ['title','content','description','category_id','thumbnail'];
+	
 	//Use this trait for Slug
 	use Sluggable;
 	/**
@@ -29,10 +32,20 @@ class Post extends Model
     }
 	
 	public function tags(){
-		return $this->belongsToMany(Tag::class);
+		//Set data in the pivot table 'post_tag' withTimestamps() - created_at, updated_at
+		return $this->belongsToMany(Tag::class)->withTimestamps();
 	}
 	
 	public function category(){
 		return $this->belongsTo(Category::class);
+	}
+	
+	//Use this method in View template for a post edit
+	public function getImage(){
+		//Message is absent
+		if(!$this->thumbnail){
+			return false;
+		}
+		return asset("downloads/{$this->thumbnail}");
 	}
 }
