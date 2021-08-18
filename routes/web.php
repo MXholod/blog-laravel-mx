5dfbd768a57d920feb6e7927e63f59340dcfecd8
Michael Holod <git_ara@ukr.net>
2021-08-19 01:06:42 +0300

    Added route for managing posts

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\MainController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\TagController;
+use App\Http\Controllers\Admin\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,4 +29,5 @@ Route::prefix('management')->group(function(){
 	Route::get('/', [MainController::class, 'index'])->name('admin.index');
 	Route::resource('/categories', CategoryController::class);
 	Route::resource('/tags', TagController::class);
+	Route::resource('/posts', PostController::class);
 });
