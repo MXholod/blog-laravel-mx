e925ddb44fa236cf50af3dbbbe9cfdbed225c6b2
Michael Holod <git_ara@ukr.net>
2021-08-26 00:44:24 +0300

    The template for user login completed

---
+++ resources/views/user/login.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 | Login Page</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <link rel="stylesheet" href="{{ mix('assets/admin/css/admin.css') }}" />
+</head>
+<body class="hold-transition register-page">
+<!-- Errors/Success -->
+  @include('templates.errors_success_template')
+<!-- Errors/Succes -->
+<div class="register-box">
+  <div class="register-logo">
+    <b>User may login here</b>
+  </div>
+
+  <div class="card">
+    <div class="card-body register-card-body">
+      <form action="{{ route('login') }}" method="post">
+		@csrf
+        <div class="input-group mb-3">
+          <input type="email" class="form-control" placeholder="Email" name="email">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" class="form-control" placeholder="Password" name="password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          
+          <!-- /.col -->
+          <div class="col-4 offset-8">
+            <button type="submit" class="btn btn-primary btn-block">Login</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+    </div>
+    <!-- /.form-box -->
+  </div><!-- /.card -->
+</div>
+<!-- /.register-box -->
+<script src="{{ mix('assets/admin/js/admin.js') }}"></script>
+</body>
+</html>
