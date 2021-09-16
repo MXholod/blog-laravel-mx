bcb6c29ae108b138cfdbda2d621bfc09868b5ab7
Michael Holod <git_ara@ukr.net>
2021-09-16 20:12:07 +0300

    Block menu items has been added to the administration sidebar for image sliders

--- resources/views/admin/parent_templates/main_sidebar_template.blade.php
+++ resources/views/admin/parent_templates/main_sidebar_template.blade.php
@@ -46,6 +46,26 @@
               <li class="nav-item has-treeview">
                 <a href="#" class="nav-link">
                   <i class="right fas fa-angle-left"></i>
+                  <p>Slider gallery</p>
+                </a>
+				<ul class="nav nav-treeview">
+					<li class="nav-item">
+						<a class="nav-link" href="{{ route('slider.index') }}">
+							<i class="far fa-circle nav-icon"></i>
+							Slider list
+						</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="{{ route('slider.create') }}">
+							<i class="far fa-circle nav-icon"></i>
+							Create slider item
+						</a>
+					</li>
+				</ul>
+              </li>
+			  <li class="nav-item has-treeview">
+                <a href="#" class="nav-link">
+                  <i class="right fas fa-angle-left"></i>
                   <p>Categories</p>
                 </a>
 				<ul class="nav nav-treeview">
