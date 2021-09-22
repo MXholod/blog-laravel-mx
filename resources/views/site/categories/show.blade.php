4d2e73f478173c7a8df8a0352c4d9b46571c55f5
Michael Holod <git_ara@ukr.net>
2021-09-22 19:29:32 +0300

    The view template to display posts according to the chosen category has been created

---
+++ resources/views/site/categories/show.blade.php
@@ -0,0 +1,60 @@
+@extends('site.layouts.main_layout')
+
+@section('title', $category->title)
+
+@section('content')
+<!--Breadcrumbs-->
+<div class="col-lg-12 top2">
+    <div class="container">
+		<ul class="breadcrumb">
+			<li><a href="{{ route('home') }}">Home</a></li>
+			<li>
+				<a href="{{ route('categories.single', ['slug' => $category->slug])  }}">
+					{{ $category->title }}
+				</a>
+			</li>
+		</ul>
+	</div>
+</div>
+<!--Breadcrumbs-->
+<div class="col-md-8">
+    <h1>Posts in {{ $category->title }}</h1>
+	@foreach($posts as $post)
+		<div class="col-md-12">
+			<h1>{{ $post->title }}</h1>
+			<div class="entry-meta table">
+				<span> Category: {{ $category->title }} </span>
+				<span> / </span>
+				<span>
+					Views: {{ $post->views }}
+				</span>
+				<span> / </span>
+				<span> Post created: {{ $post->setPostDate() }} </span>
+			</div>
+			<div>
+				@if($post->thumbnail)
+					<img src="{{ asset('downloads/'.$post->thumbnail) }}" class="img-responsive" alt="fashion">
+				@endif
+			</div>
+			<div class="media">
+				{!! $post->description !!}
+			</div>
+			<div class="read-more padding text-center">
+				<a class="btn btn-default btn-hover" href="{{ route('posts.single', ['slug'=>$post->slug]) }}" role="button">Read More</a>
+			</div>
+		</div>
+	@endforeach
+	
+	<!--Pagniation-->
+	@if(Request::is('category/'.$category->slug))
+	    <div class="container">
+			<div class="row table">
+			   <div class="text-center col-lg-8">
+					{{ $posts->links() }}
+				</div>
+			</div>
+	    </div>
+	@endif
+	<!--Pagniation End-->  
+</div>
+@endsection
