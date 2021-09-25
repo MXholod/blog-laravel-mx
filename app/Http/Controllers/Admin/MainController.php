fa8b7a230352cf977589af3b2b06c621ed774623
Michael Holod <git_ara@ukr.net>
2021-09-26 02:10:59 +0300

    Home admin page displays amount of all: Categories, Posts, Tags and Sliders

--- app/Http/Controllers/Admin/MainController.php
+++ app/Http/Controllers/Admin/MainController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Tag;
+use App\Models\Slider;
 
 class MainController extends Controller
 {
@@ -11,6 +15,16 @@ class MainController extends Controller
 	public function index(){
 		
 		$title = "Main admin page";
-		return view('admin.index', ['title' => $title]);
+		$categories = Category::all();
+		$posts = Post::all();
+		$tags = Tag::all();
+		$sliders = Slider::all();
+		return view('admin.index', [
+			'title' => $title,
+			'categories'=> $categories,
+			'posts' => $posts,
+			'tags' => $tags,
+			'sliders' => $sliders
+		]);
 	}
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -9,12 +9,6 @@
           <div class="col-sm-6">
             <h1>Main Page</h1>
           </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Blank Page</li>
-            </ol>
-          </div>
         </div>
       </div><!-- /.container-fluid -->
     </section>
@@ -25,23 +19,40 @@
       <!-- Default box -->
       <div class="card">
         <div class="card-header">
-          <h3 class="card-title">Title</h3>
+          <h3 class="card-title">{{ $title }}</h3>
 
           <div class="card-tools">
             <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
               <i class="fas fa-minus"></i>
             </button>
-            <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-              <i class="fas fa-times"></i>
-            </button>
           </div>
         </div>
         <div class="card-body">
-          Start creating your amazing application!
+			<div class="container">
+			  <b class="ml-2 pr-2">Total amount of:</b> Categories, Posts, Tags and Sliders in this application
+			  <table class="table table-striped table-bordered mt-3">
+				  <thead>
+					<tr>
+					  <th scope="col" style="width:25%">Categories</th>
+					  <th scope="col" style="width:25%">Posts</th>
+					  <th scope="col" style="width:25%">Tags</th>
+					  <th scope="col" style="width:25%">Slides</th>
+					</tr>
+				  </thead>
+				  <tbody>
+					<tr>
+					  <th scope="row">{{ $categories->count() }}</th>
+					  <td>{{ $posts->count() }}</td>
+					  <td>{{ $tags->count() }}</td>
+					  <td>{{ $sliders->count() }}</td>
+					</tr>
+				  </tbody>
+				</table>
+			</div>
         </div>
         <!-- /.card-body -->
         <div class="card-footer">
-          Footer
+          
         </div>
         <!-- /.card-footer-->
       </div>
