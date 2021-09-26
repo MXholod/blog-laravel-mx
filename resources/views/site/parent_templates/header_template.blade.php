4f83900c57a9109d89daf476cdcb9c8e163bb1c5
Michael Holod <git_ara@ukr.net>
2021-09-26 19:27:41 +0300

    Two blade directives: @auth and @guest are used in header to show user profile block

--- resources/views/site/parent_templates/header_template.blade.php
+++ resources/views/site/parent_templates/header_template.blade.php
@@ -3,24 +3,29 @@
     	<nav class="navbar navbar-inverse" role="navigation">
         	<div class="navbar-header">
             	<button type="button" id="nav-toggle" class="navbar-toggle" data-toggle="collapse" data-target="#main-nav"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-                <a href="{{ route('home') }}" class="navbar-brand scroll-top logo animated bounceInLeft rollIn"><b><i class="fa fa-html5">Your Logo</i></b></a></div>				
-              <div id="main-nav" class="collapse navbar-collapse">
-                <ul class="nav navbar-nav" id="mainNav">
-                  <li class="active"><a href="{{ route('home') }}">Home</a></li>
-                  <li><a href="about.html">About Us</a></li>
-                              <li class="dropdown">
-                               <a href="#" data-toggle="dropdown" class="dropdown-toggle">Dropdown <b class="caret"></b></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="single-post.html">Single-Post</a></li>
-                                    <li><a href="gallery.html">Gallery</a></li>
-                                    <li><a href="full-page.html">Full Page</a></li>
-                                    <li><a href="#">Page4</a></li>
-                                </ul>
-                            </li>
-                  <li><a href="blog.html">Blog</a></li>
-                  <li><a href="contact.html">Contact Us</a></li>
+                <a href="{{ route('home') }}" class="navbar-brand scroll-top logo animated bounceInLeft rollIn"><b><i class="fa fa-html5">Your Logo</i></b></a>
+			</div>				
+            <div id="main-nav" class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+					<li class="dropdown">
+                        <a href="#" data-toggle="dropdown" class="dropdown-toggle">Profile<b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            @auth
+								<li><a href="{{ route('logout') }}">Log out</a></li>
+							@endauth
+							@guest
+								<li><a href="{{ route('register.create') }}">Sign up</a></li>
+								<li><a href="{{ route('login.create') }}">Sign in</a></li>
+							@endguest
+                        </ul>
+					</li>
                 </ul>
-              </div>    
+				<ul class="nav navbar-nav" id="mainNav">
+                  <li class="active"><a href="{{ route('home') }}">Home</a></li>
+                  <li><a href="#">About Us</a></li>
+                  <li><a href="#">Contact Us</a></li>
+                </ul>
+            </div>    
         </nav>
     </div>
 </header>
