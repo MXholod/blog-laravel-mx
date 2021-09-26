2e6b526f81248614f0a74e09ccfa868491c92172
Michael Holod <git_ara@ukr.net>
2021-09-27 00:22:45 +0300

    Two templates: 'sign in' and 'sign up' have been changed. Now they use main site layout to display their forms

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -1,75 +1,90 @@
-<!DOCTYPE html>
+@extends('site.layouts.main_layout')
+<!--<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Registration Page</title>
+  <title>AdminLTE 3 | Registration Page</title>-->
 
   <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!--<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <link rel="stylesheet" href="{{ mix('assets/admin/css/admin.css') }}" />
 </head>
-<body class="hold-transition register-page">
+<body class="hold-transition register-page">-->
+
+@push('css')
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <link rel="stylesheet" href="{{ mix('assets/site/css/admin.css') }}" />
+@endpush
+
+@section('content')
 <!-- Errors/Success -->
   @include('site.parent_templates.errors_success_template')
 <!-- Errors/Succes -->
-<div class="register-box">
-  <div class="register-logo">
-    <b>User registration</b>
-  </div>
 
-  <div class="card">
-    <div class="card-body register-card-body">
-      <form action="{{ route('register.store') }}" method="post">
-		@csrf
-        <div class="input-group mb-3">
-          <input type="text" class="form-control" placeholder="User name" name="name" value="{{ old('name') }}">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-user"></span>
-            </div>
-          </div>
-        </div>
-        <div class="input-group mb-3">
-          <input type="email" class="form-control" placeholder="Email" name="email" value="{{ old('email') }}">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
-            </div>
-          </div>
-        </div>
-        <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Password" name="password">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
-            </div>
-          </div>
-        </div>
-        <div class="input-group mb-3">
-          <input type="password" name="password_confirmation" class="form-control" placeholder="Retype password">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          
-          <!-- /.col -->
-          <div class="col-4 offset-8">
-            <button type="submit" class="btn btn-primary btn-block">Register</button>
-          </div>
-          <!-- /.col -->
-        </div>
-      </form>
+<div class="col-lg-8">
+	<div class="register-box sign-up">
+	  <div class="register-logo">
+		<b>Sign up</b>
+	  </div>
 
-      <a href="#" class="text-center">I already have a membership</a>
-    </div>
-    <!-- /.form-box -->
-  </div><!-- /.card -->
+	  <div class="card">
+		<div class="card-body register-card-body">
+		  <form action="{{ route('register.store') }}" method="post">
+			@csrf
+			<div class="input-group mb-3">
+			  <input type="text" class="form-control" placeholder="User name" name="name" value="{{ old('name') }}">
+			  <div class="input-group-append">
+				<div class="input-group-text">
+				  <span class="fas fa-user"></span>
+				</div>
+			  </div>
+			</div>
+			<div class="input-group mb-3">
+			  <input type="email" class="form-control" placeholder="Email" name="email" value="{{ old('email') }}">
+			  <div class="input-group-append">
+				<div class="input-group-text">
+				  <span class="fas fa-envelope"></span>
+				</div>
+			  </div>
+			</div>
+			<div class="input-group mb-3">
+			  <input type="password" class="form-control" placeholder="Password" name="password">
+			  <div class="input-group-append">
+				<div class="input-group-text">
+				  <span class="fas fa-lock"></span>
+				</div>
+			  </div>
+			</div>
+			<div class="input-group mb-3">
+			  <input type="password" name="password_confirmation" class="form-control" placeholder="Retype password">
+			  <div class="input-group-append">
+				<div class="input-group-text">
+				  <span class="fas fa-lock"></span>
+				</div>
+			  </div>
+			</div>
+			<div class="row">
+			  
+			  <!-- /.col -->
+			  <div class="col-4 offset-8">
+				<button type="submit" class="btn btn-primary sign-up__btn">Register</button>
+			  </div>
+			  <!-- /.col -->
+			</div>
+		  </form>
+		</div>
+		<!-- /.form-box -->
+	  </div><!-- /.card -->
+	</div>
 </div>
 <!-- /.register-box -->
-<script src="{{ mix('assets/admin/js/admin.js') }}"></script>
+@push('scripts_bottom')
+	<script src="{{ mix('assets/admin/js/admin.js') }}"></script>
+@endpush
+
+@endsection
+<!--
 </body>
-</html>
+</html>-->
--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -1,57 +1,73 @@
-<!DOCTYPE html>
+@extends('site.layouts.main_layout')
+<!--<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Login Page</title>
+  <title>AdminLTE 3 | Registration Page</title>-->
 
   <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!--<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <link rel="stylesheet" href="{{ mix('assets/admin/css/admin.css') }}" />
 </head>
-<body class="hold-transition register-page">
+<body class="hold-transition register-page">-->
+
+@push('css')
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <link rel="stylesheet" href="{{ mix('assets/site/css/admin.css') }}" />
+@endpush
+
+@section('content')
 <!-- Errors/Success -->
   @include('site.parent_templates.errors_success_template')
 <!-- Errors/Succes -->
-<div class="register-box">
-  <div class="register-logo">
-    <b>User may login here</b>
-  </div>
+<div class="col-lg-8">
+	<div class="register-box sign-in">
+	  <div class="register-logo">
+		<b>Sign in</b>
+	  </div>
 
-  <div class="card">
-    <div class="card-body register-card-body">
-      <form action="{{ route('login') }}" method="post">
-		@csrf
-        <div class="input-group mb-3">
-          <input type="email" class="form-control" placeholder="Email" name="email">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
-            </div>
-          </div>
-        </div>
-        <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Password" name="password">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          
-          <!-- /.col -->
-          <div class="col-4 offset-8">
-            <button type="submit" class="btn btn-primary btn-block">Login</button>
-          </div>
-          <!-- /.col -->
-        </div>
-      </form>
-    </div>
-    <!-- /.form-box -->
-  </div><!-- /.card -->
+	  <div class="card">
+		<div class="card-body register-card-body">
+		  <form action="{{ route('login') }}" method="post">
+			@csrf
+			<div class="input-group mb-3">
+			  <input type="email" class="form-control" placeholder="Email" name="email">
+			  <div class="input-group-append">
+				<div class="input-group-text">
+				  <span class="fas fa-envelope"></span>
+				</div>
+			  </div>
+			</div>
+			<div class="input-group mb-3">
+			  <input type="password" class="form-control" placeholder="Password" name="password">
+			  <div class="input-group-append">
+				<div class="input-group-text">
+				  <span class="fas fa-lock"></span>
+				</div>
+			  </div>
+			</div>
+			<div class="row">
+			  
+			  <!-- /.col -->
+			  <div class="col-4 offset-8">
+				<button type="submit" class="btn btn-primary sign-in__btn">Login</button>
+			  </div>
+			  <!-- /.col -->
+			</div>
+		  </form>
+		</div>
+		<!-- /.form-box -->
+	  </div><!-- /.card -->
+	</div>
 </div>
 <!-- /.register-box -->
-<script src="{{ mix('assets/admin/js/admin.js') }}"></script>
+@push('scripts_bottom')
+	<script src="{{ mix('assets/admin/js/admin.js') }}"></script>
+@endpush
+
+@endsection
+<!--
 </body>
-</html>
+</html>-->
