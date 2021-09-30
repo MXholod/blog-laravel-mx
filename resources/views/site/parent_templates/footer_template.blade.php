c1cbc030df41e0b5a4570bb35d6520c0656f84f8
Michael Holod <git_ara@ukr.net>
2021-09-30 18:47:01 +0300

    View templates header and footer receive data from view service provider to display menu items

--- resources/views/site/parent_templates/footer_template.blade.php
+++ resources/views/site/parent_templates/footer_template.blade.php
@@ -15,13 +15,19 @@
                 </div>
                 <div class="col-lg-4">
                 <h1>Like Us</h1>
-                	<div class="fb-page" data-href="https://www.facebook.com/themesrefinery" data-hide-cover="false" data-show-facepile="true" data-show-posts="false"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/themesrefinery"><a href="https://www.facebook.com/themesrefinery">ThemesRefinery</a></blockquote></div></div>
                     <div class="text-center">
                         <a href="https://www.facebook.com/themesrefinery"><i class="fa fa-facebook square"></i></a>
                         <a href="https://twitter.com/themesrefinery"><i class="fa fa-twitter square"></i></a>
                         <a href="#"><i class="fa fa-github square"></i></a>
                         <a href="https://plus.google.com/b/101108467301668768757/+Themesrefinery57/posts"><i class="fa fa-google-plus square"></i></a>
                     </div>
+					<ul class="nav nav-pills" style="margin-top:2em;">
+					  @foreach($footer_links as $footer_link)
+						<li class="nav-item">
+							<a href="{{ $footer_link['slug'] }}" class="nav-link active" aria-current="page">{{ $footer_link['title'] }}</a>
+						</li>
+					  @endforeach
+					</ul>
                 </div>
                 <div class="col-lg-4">
                 	<h1>Recent Posts</h1>
@@ -37,6 +43,6 @@
                 </div>
     </div>
     <div class="col-lg-12 top2 bottom2">
-    	<div class="text-center">Copy Right &copy; 2015.Design By <a href="http://www.themesrefinery.net/"><i class="fa  fa-file-code">Themesrefinery</i></a></div>
+    	<div class="text-center">Copy Right &copy; @php echo date('Y') @endphp Design By Mike</div>
     </div>
 </footer>
--- resources/views/site/parent_templates/header_template.blade.php
+++ resources/views/site/parent_templates/header_template.blade.php
@@ -22,8 +22,9 @@
                 </ul>
 				<ul class="nav navbar-nav" id="mainNav">
                   <li class="active"><a href="{{ route('home') }}">Home</a></li>
-                  <li><a href="#">About Us</a></li>
-                  <li><a href="#">Contact Us</a></li>
+				  @foreach($header_links as $header_link)
+					<li><a href="{{ $header_link['slug'] }}">{{ $header_link['title'] }}</a></li>
+				  @endforeach
                 </ul>
             </div>    
         </nav>
