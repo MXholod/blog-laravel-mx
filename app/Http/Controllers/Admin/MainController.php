124ef74b6b320dd1de5ffb24dc0b1a8c84586459
Michael Holod <git_ara@ukr.net>
2021-10-21 02:10:35 +0300

    The main admin page controller has been changed. It always displays the first row of 'logos' table, which contains the site logo settings

--- app/Http/Controllers/Admin/MainController.php
+++ app/Http/Controllers/Admin/MainController.php
@@ -9,6 +9,7 @@ use App\Models\Post;
 use App\Models\Tag;
 use App\Models\Slider;
 use App\Models\Page;
+use App\Models\Logo;
 
 class MainController extends Controller
 {
@@ -21,13 +22,17 @@ class MainController extends Controller
 		$tags = Tag::all();
 		$sliders = Slider::all();
 		$pages = Page::all();
+		//Get the site logo
+		$logo = Logo::all();
+		$logo = $logo[0] ?? $logo;
 		return view('admin.index', [
 			'title' => $title,
 			'categories'=> $categories,
 			'posts' => $posts,
 			'tags' => $tags,
 			'sliders' => $sliders,
-			'pages' => $pages
+			'pages' => $pages,
+			'logo' => $logo //Always passes the first row of the table
 		]);
 	}
 }
