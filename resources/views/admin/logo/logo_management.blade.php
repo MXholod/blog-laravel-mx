ae9dd8f68c6a39cc57234852831c5a8b42b2091f
Michael Holod <git_ara@ukr.net>
2021-10-22 20:02:55 +0300

    Template showing site logo in the admin panel changed. The functionality for switching between show or hide logo and choosing small or medium logo size is added. It works using AJAX

--- resources/views/admin/logo/logo_management.blade.php
+++ resources/views/admin/logo/logo_management.blade.php
@@ -8,6 +8,9 @@
 				<input type="file" class="form-control" id="logoImage" name="logo" />
 			</div>
 			@if($logo->count())
+				<div class="spinner-border text-primary" role="status" id="logoLoadingVisibility">
+				  <span class="sr-only">Loading...</span>
+				</div>	
 				<div class="form-check">
 				  <input class="form-check-input" type="radio" name="logo_img_display" id="showLogo" value="0" {{ $logo->logo_img_display == 0 ? 'checked' : ''  }} />
 				  <label class="form-check-label" for="showLogo">
@@ -24,6 +27,9 @@
 					<input class="form-control" type="text" placeholder="Logo title" aria-label="default input example" name="logo_title" value="{{ $logo->logo_title }}" />
 				</label>
 				<div class="logo-block__logo-size">
+					<div class="spinner-border text-primary" role="status" id="logoLoadingSize">
+					  <span class="sr-only">Loading...</span>
+					</div>
 					<label>
 						<input type="radio" name="logo_size" {{ $logo->logo_size == 's' ? 'checked' : ''  }} value="s" /> 
 						Width: 115px, Height: 50px <b>(Small)</b>
@@ -33,7 +39,7 @@
 						Width: 160px, Height: 80px <b>(Medium)</b>
 					</label>
 				</div>
-				<input type="hidden" name="rewrite_logo" value="{{ $logo->id }}" />
+				<input type="hidden" name="rewrite_logo" value="{{ $logo->id }}" id="logoId" />
 				<input type="submit" value="Replace logo with another one" class="btn btn-secondary btn-sm" role="button" />
 			@else
 				<div class="form-check">
@@ -79,3 +85,94 @@
 		@endif
 	</div>
 </div>
+
+@push('scripts_bottom')
+<script type="text/javascript">
+	window.addEventListener('load',function(){
+		//Logo ID exists if the logo image is presented on the page
+		let logoId = document.getElementById('logoId');
+		//Get current Laravel route
+		let urlVisibility = "{{route('logo.visibility')}}";
+		let urlSize = "{{route('logo.size')}}";
+		//Show hide logo
+		let showLogo = document.getElementById('showLogo');
+		let hideLogo = document.getElementById('hideLogo');
+		let logoLoadingVisibility = document.getElementById('logoLoadingVisibility');
+		//Change size between 'small' and 'medium'
+		let sizeInputs = document.querySelectorAll(".logo-block__logo-size input[type='radio']");
+		
+		function setEvent(elem, event, funcToDo){
+			if(elem){
+				elem.addEventListener(event, funcToDo);
+			}else{
+				//console.log("Element is absent");
+			}
+		}
+		//Show logo
+		setEvent(showLogo, 'change', function(){
+			if(!logoId?.value) return;
+			showLogo.disabled = true;
+			hideLogo.disabled = true;
+			logoLoadingVisibility.style.display = "block";
+			makeRequest({'group': 'visibility','value': this.value, 'id':logoId.value}, urlVisibility);
+		});
+		//Hide logo
+		setEvent(hideLogo, 'change', function(){
+			if(!logoId?.value) return;
+			showLogo.disabled = true;
+			hideLogo.disabled = true;
+			logoLoadingVisibility.style.display = "block";
+			makeRequest({'group': 'visibility','value': this.value, 'id':logoId.value}, urlVisibility);
+		});
+		//Logo size small
+		setEvent(sizeInputs[0], 'change', function(){
+			if(!logoId?.value) return;
+			sizeInputs[0].disabled = true;
+			sizeInputs[1].disabled = true;
+			logoLoadingSize.style.display = "block";
+			makeRequest({'group': 'size','value': this.value, 'id':logoId.value}, urlSize);
+		});
+		//Logo size medium
+		setEvent(sizeInputs[1], 'change', function(){
+			if(!logoId?.value) return;
+			sizeInputs[0].disabled = true;
+			sizeInputs[1].disabled = true;
+			logoLoadingSize.style.display = "block";
+			makeRequest({'group': 'size','value': this.value, 'id':logoId.value}, urlSize);
+		});
+		
+		//AJAX request
+		function makeRequest(data, url){
+			$.ajaxSetup({
+			  headers: {
+				'X-CSRF-TOKEN': '{{csrf_token()}}'
+			  }
+			});
+			$.ajax({
+                url,
+                type: 'PATCH',
+                data,
+                success: function(res) {
+					window.setTimeout(()=>{
+						//Hide loader for logo visibility
+						if(logoLoadingVisibility.style.display === "block"){
+							logoLoadingVisibility.style.display = "none";
+							showLogo.disabled = false;
+							hideLogo.disabled = false;
+						}
+						//Hide loader for logo size
+						if(logoLoadingSize.style.display === "block"){
+							logoLoadingSize.style.display = "none";
+							sizeInputs[0].disabled = false;
+							sizeInputs[1].disabled = false;
+						}
+					},2000);
+                },
+				error: function(res) {
+					console.log("Error ",res);
+                }
+			});
+		}
+	});
+</script>
+@endpush
