6e82a38a75f8f1ab39486889eeb22ace49038a37
Michael Holod <git_ara@ukr.net>
2021-11-01 18:48:03 +0200

    The 'CategoryController' controller in the admin panel has been modified. It implements AJAX functionality to make AJAX requests for creating, updating, and deleting widgets

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Category;
+use App\Models\Widget;
 
 class CategoryController extends Controller
 {
@@ -57,6 +58,40 @@ class CategoryController extends Controller
 		//Flash message another way 'with()' method
 		return redirect()->route('categories.index')->with('success', "The '{$categoryTitle}' has been added");
     }
+	
+	//AJAX request
+	public function store_widget(Request $request){
+		//Try to add a widget by Ajax
+		if($request->ajax()){
+			//If widget is absent
+			if(is_null($request->input('title')) || is_null($request->input('fullText'))){
+				return response()->json([
+					"incorrect" => "Data is incorrect"
+				]);
+			}
+			$entityId = (int)$request->input('entityId');
+			//Find category
+			$category = Category::find($entityId);
+			//Create widget
+			$widget = new Widget;
+			$widget->title = $request->input('title');
+			$widget->full_text = json_decode($request->input('fullText'));
+			//Save widget with the category
+			$category->widgets()->save($widget);
+			
+			/*return response()->json([
+				"status" => "Widget added to the page"
+			]);*/
+			
+			//Get all category widgets
+			$category = Category::where('slug', $category->slug)->first();
+			$widgets = $category->widgets()->orderBy('created_at', 'desc')->get();//->paginate($this->commentsAmount);
+			//Set two route names according to the entity (Category)
+			$routeUpdate = 'category.widget.update';
+			$routeDelete = 'category.widget.destroy';
+			return view('admin.widgets.widget_single_block', compact('entityId', 'routeUpdate', 'routeDelete', 'widgets'))->render();
+		}
+	}
 
     /**
      * Display the specified resource.
@@ -108,6 +143,38 @@ class CategoryController extends Controller
 		
 		return redirect()->route('categories.index')->with('success', "The '{$categoryTitle}' has been updated");
     }
+	
+	//AJAX request
+	public function update_widget(Request $request){
+		//Try to update a widget by Ajax
+		if($request->ajax()){
+			if($request->isMethod('patch')){
+				//If widget is absent
+				if(is_null($request->input('title')) || is_null($request->input('fullText'))){
+					return response()->json([
+						"incorrect" => "Data is incorrect"
+					]);
+				}
+				$idWidget = (int)$request->input('widgetId');
+				//Find widget;
+				$widget = Widget::find($idWidget);
+				$widget->title = $request->input('title');
+				$widget->full_text = json_decode($request->input('fullText'));
+				$widget->save();
+				
+				$entityId = (int)$request->input('entityId');
+				//Find category
+				$category = Category::find($entityId);
+				//Get all category widgets
+				$category = Category::where('slug', $category->slug)->first();
+				$widgets = $category->widgets()->orderBy('created_at', 'desc')->get();//->paginate($this->commentsAmount);
+				//Set two route names according to the entity (Category)
+				$routeUpdate = 'category.widget.update';
+				$routeDelete = 'category.widget.destroy';
+				return view('admin.widgets.widget_single_block', compact('entityId', 'routeUpdate', 'routeDelete', 'widgets'))->render();
+			}
+		}
+	}
 
     /**
      * Remove the specified resource from storage.
@@ -132,4 +199,35 @@ class CategoryController extends Controller
 			return redirect()->route('categories.index')->with('error', "The category '{$category->title}' '{$postsStr}'");
 		}
     }
+	
+	//AJAX request
+	public function destroy_widget(Request $request){
+		//Try to deletion a widget by Ajax
+		if($request->ajax()){
+			if($request->isMethod('delete')){
+				//If widget is absent
+				if(is_null($request->input('entityId')) || is_null($request->input('widgetId'))){
+					return response()->json([
+						"incorrect" => "Data is incorrect"
+					]);
+				}
+				$idWidget = (int)$request->input('widgetId');
+				$entityId = (int)$request->input('entityId');
+				//Find category
+				$category = Category::find($entityId);
+				//Delete widget in 'widgets' table 
+				$category->widgets()->where('id', '=', $idWidget)->delete();
+				// Detach a single widget from the category...
+				$category->widgets()->detach($idWidget);
+				
+				//Get all category widgets
+				$category = Category::where('slug', $category->slug)->first();
+				$widgets = $category->widgets()->orderBy('created_at', 'desc')->get();//->paginate($this->commentsAmount);
+				//Set two route names according to the entity (Category)
+				$routeUpdate = 'category.widget.update';
+				$routeDelete = 'category.widget.destroy';
+				return view('admin.widgets.widget_single_block', compact('entityId', 'routeUpdate', 'routeDelete', 'widgets'))->render();
+			}
+		}
+	}
 }
