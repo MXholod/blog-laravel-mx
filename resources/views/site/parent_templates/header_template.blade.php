919455f258e5201bdfc487314e381e557707c729
Michael Holod <git_ara@ukr.net>
2021-11-02 22:27:35 +0200

    Setting an active link when clicking on it in the main menu in the header and also displays the signed user name near the logout button

--- resources/views/site/parent_templates/header_template.blade.php
+++ resources/views/site/parent_templates/header_template.blade.php
@@ -33,12 +33,15 @@
 				</a>
 			</div>				
             <div id="main-nav" class="collapse navbar-collapse">
-                <ul class="nav navbar-nav">
+                <ul class="nav navbar-nav" id="mainNavDropDown">
 					<li class="dropdown">
                         <a href="#" data-toggle="dropdown" class="dropdown-toggle">Profile<b class="caret"></b></a>
-                        <ul class="dropdown-menu">
+                        <ul class="dropdown-menu" id="mainNavDropDownMenu">
                             @auth
-								<li><a href="{{ route('logout') }}">Log out</a></li>
+								<li>
+									<span class="user-name">User: {{ auth()->user()->name }}</span>
+									<a href="{{ route('logout') }}">Log out</a>
+								</li>
 							@endauth
 							@guest
 								<li><a href="{{ route('register.create') }}">Sign up</a></li>
@@ -57,3 +60,72 @@
         </nav>
     </div>
 </header>
+@push('scripts_bottom')
+	<script>
+		$(document).ready(function(){
+			if(!window.sessionStorage.getItem('activeHeaderMenu')){
+				let firstAText = $("#mainNav a").first().text().toLowerCase().split(' ').join('-');
+				//Initial first menu item as active item if 'sessionStorage' is empty
+				window.sessionStorage.setItem('activeHeaderMenu', firstAText);
+			}else{
+				//Remove all 'active' classes on load
+				$("#mainNav").find(".active").removeClass("active");
+				$("#mainNavDropDown").first().removeClass("active");
+				$("#mainNavDropDownMenu").find(".active").removeClass("active");
+				//Get current 'active' from 'sessionStorage'
+				let activeItemAfterSignUp = window.sessionStorage.getItem('activeHeaderMenu');
+				if(activeItemAfterSignUp === 'sign-up' && $('#mainNavDropDownMenu').children().length < 2){
+					//Set 'home' as active after signed up
+					let firstAText = $("#mainNav a").first().text().toLowerCase().split(' ').join('-');
+					window.sessionStorage.setItem('activeHeaderMenu', firstAText);
+				}
+				//Get current 'active' from 'sessionStorage'
+				let activeItem = window.sessionStorage.getItem('activeHeaderMenu');
+				//Set current 'active' class from previous click
+				$("#mainNav a").each(function(){
+					//If find 'a' by text
+					if(activeItem === $(this).text().toLowerCase().split(' ').join('-')){
+						$(this).parent().addClass("active");
+					}
+				});
+				//Set current 'active' class from previous click
+				$("#mainNavDropDownMenu a").each(function(){
+					//If find 'a' by text
+					if(activeItem === $(this).text().toLowerCase().split(' ').join('-')){
+						$("#mainNavDropDown").first().addClass("active");
+						$(this).parent().addClass("active");
+					}
+				});
+			}
+			//Get root url and compare with logo url
+			$('.logo.animated.bounceInLeft').on("click", function(){
+				let logoHref = $(this).attr('href');
+				if("{{ route('home') }}" === logoHref){
+					//"{{ url()->full() }}"//Laravel helper
+					//Set 'home' as active after signed up
+					let firstAText = $("#mainNav a").first().text().toLowerCase().split(' ').join('-');
+					window.sessionStorage.setItem('activeHeaderMenu', firstAText);
+				}
+			});
+			//Click on each 'a' in main menu
+			$("#mainNav a").on("click", function(){
+				//Get an 'a' element text
+				let firstAText = $(this).text().toLowerCase().split(' ').join('-');
+				//Initial first menu item as active item if 'sessionStorage' is empty
+				window.sessionStorage.setItem('activeHeaderMenu', firstAText);
+			});
+			//Click on each 'a' drop down menu
+			$("#mainNavDropDownMenu a").on("click", function(){
+				//Get an 'a' element text
+				let firstAText = $(this).text().toLowerCase().split(' ').join('-');
+				if(firstAText === 'log-out'){
+					//Set register page as active item if user logs out
+					window.sessionStorage.setItem('activeHeaderMenu', 'sign-up');
+					return true;
+				}
+				//Initial first menu item as active item if 'sessionStorage' is empty
+				window.sessionStorage.setItem('activeHeaderMenu', firstAText);
+			});
+		});	
+	</script>
+@endpush()
