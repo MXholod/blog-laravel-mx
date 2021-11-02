9a47dd85b4adb94535e279758dc9f2fcad8bdfd1
Michael Holod <git_ara@ukr.net>
2021-11-02 23:32:25 +0200

    Category controller changed. It displays widgets for 'category'

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -11,8 +11,14 @@ class CategoryController extends Controller
 	public function show($slug){
 		//Get requested 'category' or 404 error
 		$category = Category::where('slug', $slug)->firstOrFail();
+		//Get widgets for category
+		if(!is_null($category)){
+			$categoryWidgets = Category::find($category->id);//->get();
+		}
+		//Form an array of widgets where key is a 'title' and 'value' is a 'full_text'
+		$category_widgets = $categoryWidgets->widgets->pluck('full_text', 'title');
 		//The posts() method is from Category Model
 		$posts = $category->posts()->orderBy('id', 'desc')->paginate(2);
-		return view('site.categories.show', compact('category', 'posts'));
+		return view('site.categories.show', compact('category', 'posts', 'category_widgets'));
 	}
 }
